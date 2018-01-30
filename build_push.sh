399ce7572bf7a74ae60105a8056b4dccb455c1be
Steve Chan <sychan168@gmail.com>
2018-01-30 15:13:51 -0800

    Add simple build/push script

---
+++ build_push.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+# Simple script that builds this image and pushes it to dockerhub, assuming
+# the account already has logged into dockerhub
+#
+
+BRANCH=`git rev-parse --abbrev-ref HEAD`
+TAG=`if [ "$BRANCH" == "master" ]; then echo "latest"; else echo $BRANCH ; fi`
+IMAGE_NAME=kbase/kb_logstash:$TAG
+docker build . -t $IMAGE_NAME
+
+docker push $IMAGE_NAME
